c81d12eec9c4330a73437eb3a6f93b6baacea144
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-16 10:20:33 -0600

    Test: ride loop and total distance

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -13,6 +13,19 @@ RSpec.describe Ride do
         expect(@ride1.name).to eq()
         expect(@ride1.distance).to eq(10.7)
         expect(@ride1.terrain).to eq(:hills)
+    end
+
+    it 'states if ride loops' do
         expect(@ride1.loop?).to be false
+
+        ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+        expect(ride2.loop?).to be true
+    end
+
+    it 'doubles total distance if no loop' do
+        ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+        expect(ride2.total_distance).to eq(14.9)
+
+        expect(@ride1.total_distance).to eq(21.4)
     end
 end
